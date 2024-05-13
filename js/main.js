d24805f75fc4e5ae251fef91f0e7f22149b865e3
David Torres <david.martinezezguerra@gmail.com>
2024-05-13 17:41:35 -0500

    seleccionar mascota enemigo

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@ function seleccionarMascota() {
     }
 }
 
+function ramdomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function seleccionarMascotaEnemigo() {
+    let numeroAleatorio = ramdomNumber(1,3)
+    if(numeroAleatorio == 1) {
+        nombreMascotaEnemigo.innerHTML = 'tatara'
+    } else if(numeroAleatorio == 2) {
+        nombreMascotaEnemigo.innerHTML = 'holala'
+    } else {
+        nombreMascotaEnemigo.innerHTML = 'keton'
+    }
+}
+
 const inputMascotaJugador = document.querySelectorAll('.mascota-radio')
 
 const nombreMascotaJugador = document.getElementById('mascota-jugador')
+const nombreMascotaEnemigo = document.getElementById('mascota-enemigo')
 const buttonMascotaJugador = document.getElementById('button-mascota')
 buttonMascotaJugador.addEventListener('click', seleccionarMascota)
