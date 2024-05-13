17558cdaae11c79a265d813ecff20c47393f03f3
David Torres <david.martinezezguerra@gmail.com>
2024-05-13 17:29:08 -0500

    seleccionar e insertar nombre de mascota jugador

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 function seleccionarMascota() {
-    alert('seleccionaste')
+    for(let mascota of inputMascotaJugador) {
+        if(mascota.checked) {
+            nombreMascotaJugador.innerHTML = mascota.id
+        }
+    }
 }
 
-const buttonMascotaJugador = document.getElementById('#button-mascota')
+const inputMascotaJugador = document.querySelectorAll('.mascota-radio')
+
+const nombreMascotaJugador = document.getElementById('mascota-jugador')
+const buttonMascotaJugador = document.getElementById('button-mascota')
 buttonMascotaJugador.addEventListener('click', seleccionarMascota)
